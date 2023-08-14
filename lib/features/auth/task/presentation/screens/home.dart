297d50ba670dd04d4dca9fcc29cab63562b93658
AmrTaher70 <amrhagiz47@gmail.com>
2023-08-15 00:00:00 +0300

    no tasks widget

--- lib/features/auth/task/presentation/screens/home.dart
+++ lib/features/auth/task/presentation/screens/home.dart
@@ -1,4 +1,9 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:up_course2/core/app.assets.dart';
+import 'package:up_course2/core/app.colors.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +15,54 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: AppColors.primary,
+          child: const Icon(CupertinoIcons.plus),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat.yMMMMd().format(DateTime.now()),
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Today',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              DatePicker(
+                DateTime.now(),
+                initialSelectedDate: DateTime.now(),
+                selectionColor: AppColors.backgroundOfDatePicker,
+                selectedTextColor: AppColors.white,
+                dayTextStyle: Theme.of(context).textTheme.displayMedium!,
+                monthTextStyle: Theme.of(context).textTheme.displayMedium!,
+                dateTextStyle: Theme.of(context).textTheme.displayMedium!,
+                height: 94,
+                width: 59,
+                onDateChange: (date) {},
+              ),
+              const SizedBox(
+                height: 11,
+              ),
+              noTask(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Center noTask() => Center(child: Image.asset(AppAssets.task));
 }
